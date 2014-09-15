b0b44d3911b00a486c0dad24368e6a97e2e6898f
Noël Plouzeau <noel.plouzeau@irisa.fr>
2014-09-15 15:18:11 +0200

    Clumsy test of DnD.
    NEEDS A BETTER EXAMPLE

---
+++ DnD1/src/dnd/Controller.java
@@ -0,0 +1,67 @@
+package dnd;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.control.TextField;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Paint;
+
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    // Acceptable suffixes for file payload in DnD
+    private List<String> validFileSuffixes = Arrays.asList(".jpg", ".gif", "png");
+
+    @FXML
+    private Pane dndTarget;
+
+    @FXML
+    private TextField messageArea;
+
+    @FXML
+    void onDndOver(DragEvent event) {
+
+        Background dndOkBackground =
+                new Background(new BackgroundFill(Paint.valueOf("green"), CornerRadii.EMPTY, Insets.EMPTY));
+        Dragboard dragBoard = event.getDragboard();
+        if (dragBoard.hasFiles()
+                && (dragBoard.getFiles().size() == 1)
+                && (isAnAcceptableFileType(getFileNameFromBoard(dragBoard)))) {
+
+            messageArea.setText(getFileNameFromBoard(dragBoard));
+            dndTarget.setBackground(dndOkBackground);
+        }
+    }
+
+
+    private boolean isAnAcceptableFileType(String path) {
+        return validFileSuffixes.stream().anyMatch(t -> path.endsWith(t));
+    }
+
+    private String getFileNameFromBoard(Dragboard dragBoard) {
+        return dragBoard.getFiles().get(0).getAbsolutePath();
+    }
+
+    /**
+     * Called to initialize a controller after its root element has been
+     * completely processed.
+     *
+     * @param location  The location used to resolve relative paths for the root object, or
+     *                  <tt>null</tt> if the location is not known.
+     * @param resources The resources used to localize the root object, or <tt>null</tt> if
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
