f39c7966301c97782fbac4a4078da48d0ccaf6aa
Noël Plouzeau <noel.plouzeau@irisa.fr>
2014-10-12 16:15:06 +0200

    Added a few more tests

--- My512/src/main/java/model/test/BoardImplTest.java
+++ My512/src/main/java/model/test/BoardImplTest.java
@@ -24,15 +24,10 @@ public class BoardImplTest {
         Assert.assertEquals(defaultBoardSize, testBoard.getSideSizeInSquares());
     }
 
-    @Test
-    public void testGetTile() throws Exception {
-        Assert.fail("test case not implemented");
-    }
-
     /*
      * Identity test for packing
      * The current and next board should be the same
-    */
+     */
     @Test
     public void testPackIntoDirection1() throws Exception {
         testBoard.loadBoard(new int[][]{{1, 0, 0}, {0, 0, 0}, {0, 0, 0}});
@@ -50,35 +45,59 @@ public class BoardImplTest {
      */
     @Test
     public void testPackIntoDirection2() throws Exception {
-        testBoard.loadBoard(new int[][]{{0,0,1},{0,0,0},{0,0,0}});
+        testBoard.loadBoard(new int[][]{{0, 0, 1}, {0, 0, 0}, {0, 0, 0}});
         testBoard.packIntoDirection(Board.Direction.LEFT);
         testBoard.commit();
-        Tile uniqueTile = testBoard.getTile(1,1);
+        Tile uniqueTile = testBoard.getTile(1, 1);
         Assert.assertNotNull(uniqueTile);
-        Assert.assertEquals(1,uniqueTile.getRank());
-        Assert.assertNull(testBoard.getTile(1,3));
+        Assert.assertEquals(1, uniqueTile.getRank());
+        Assert.assertNull(testBoard.getTile(1, 3));
     }
 
     /*
     * Check that tiles in (1,2) and (1,3) with different ranks
-    * are moved in (1,1 and (1,2)
+    * are moved in (1,1) and (1,2)
     */
     @Test
     public void testPackIntoDirection3() throws Exception {
-        testBoard.loadBoard(new int[][]{{0,1,2},{0,0,0},{0,0,0}});
+        testBoard.loadBoard(new int[][]{{0, 1, 2}, {0, 0, 0}, {0, 0, 0}});
         testBoard.packIntoDirection(Board.Direction.LEFT);
         testBoard.commit();
-        Tile uniqueTile = testBoard.getTile(1,1);
+        Tile uniqueTile = testBoard.getTile(1, 1);
         Assert.assertNotNull(uniqueTile);
-        Assert.assertEquals(1,uniqueTile.getRank());
-        Assert.assertNotNull(testBoard.getTile(1,2));
-        Assert.assertEquals(2,testBoard.getTile(1,2).getRank());
+        Assert.assertEquals(1, uniqueTile.getRank());
+        Assert.assertNotNull(testBoard.getTile(1, 2));
+        Assert.assertEquals(2, testBoard.getTile(1, 2).getRank());
     }
 
-
+    /*
+     * Check that a basic merge works,
+     * namely that (1,2),(1,3) with rank 1 get merged into (1,1) with rank 2
+     */
     @Test
-    public void testCommit() throws Exception {
-        Assert.fail("test case not implemented");
-
+    public void testPackIntoDirection4() throws Exception {
+        testBoard.loadBoard(new int[][]{{0, 1, 1}, {0, 0, 0}, {0, 0, 0}});
+        testBoard.packIntoDirection(Board.Direction.LEFT);
+        testBoard.commit();
+        Assert.assertNotNull(testBoard.getTile(1, 1));
+        Assert.assertEquals(2, testBoard.getTile(1, 1).getRank());
+        Assert.assertNull(testBoard.getTile(1, 2));
+        Assert.assertNull(testBoard.getTile(1, 3));
+    }
+    /*
+     * Check that a more complicated merge works,
+     * namely that (1,2),(1,3) with rank 1 get merged into (1,2) with rank 2
+     * and that (1,1) with rank 3 is copied into (1,1)
+     */
+    @Test
+    public void testPackIntoDirection5() throws Exception {
+        testBoard.loadBoard(new int[][]{{3, 1, 1}, {0, 0, 0}, {0, 0, 0}});
+        testBoard.packIntoDirection(Board.Direction.LEFT);
+        testBoard.commit();
+        Assert.assertNotNull(testBoard.getTile(1, 1));
+        Assert.assertEquals(3,testBoard.getTile(1,1).getRank());
+        Assert.assertNotNull(testBoard.getTile(1, 2));
+        Assert.assertEquals(2, testBoard.getTile(1, 2).getRank());
+        Assert.assertNull(testBoard.getTile(1, 3));
     }
 }
